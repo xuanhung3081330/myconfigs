af996f010aecc9c107ba8bb99c1152343592b9a0
Nguyen Xuan Hung <35716317+xuanhung3081330@users.noreply.github.com>
2025-06-28 17:28:21 +0900

    Update load_keys.sh

--- scripts/load_keys.sh
+++ scripts/load_keys.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Checks if $SSH_AUTH_SOCK exists and is a Unix domain socket
-[-S "$SSH_AUTH_SOCK" ] && ssh-add -l > /dev/null 2>&1 || ssh-add ~/.ssh/id_ed25519_github
+[ -S "$SSH_AUTH_SOCK" ] && ssh-add -l > /dev/null 2>&1 || ssh-add ~/.ssh/id_ed25519_github
