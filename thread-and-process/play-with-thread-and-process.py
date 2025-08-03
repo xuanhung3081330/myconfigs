3c9377736713ce4c3dca2c5005a5be8ca3880124
xuanhung <xuanhung3081330@gmail.com>
2025-08-03 13:19:40 +0900

    Playing with Thread

--- thread-and-process/play-with-thread-and-process.py
+++ thread-and-process/play-with-thread-and-process.py
@@ -1,25 +1,38 @@
 from multiprocessing import Process
 import os
 import time
+import threading
 
 def task(name):
-    print(f"[{name}] Process ID: {os.getpid()}")
+    print(f"[{name}] Thread ID: {threading.get_ident()}")
     print(f"[{name}] Starting work...")
     time.sleep(2)
     print(f"[{name}] Work done!")
 
 if __name__ == "__main__":
-    p1 = Process(target=task, args=("Task-1",))
-    p2 = Process(target=task, args=("Task-2",))
+    #p1 = Process(target=task, args=("Task-1",))
+    #p2 = Process(target=task, args=("Task-2",))
 
-    p1.start() # Start first Process
-    p1.join()
+    #p1.start() # Start first Process
+    #p1.join()
 
-    p2.start() # Start second process
-    p2.join()
+    #p2.start() # Start second process
+    #p2.join()
 
-    print("Main process waiting for subprocesses to finish...")
+    #print("Main process waiting for subprocesses to finish...")
     #p1.join()
     #p2.join()
 
-    print("Main process done.")
+    #print("Main process done.")
+
+    t1 = threading.Thread(target=task, args=("Thread-1",))
+    t2 = threading.Thread(target=task, args=("Thread-2",))
+
+    t1.start()
+    t2.start()
+
+    print("Main thread waiting for threads to finish...")
+    t1.join()
+    t2.join()
+
+    print("Main thread done.")
