01f63c444236e00b21b07c1ea45c4f5c38738eef
xuanhung <xuanhung3081330@gmail.com>
2025-08-03 13:03:58 +0900

    Play with multi-processing

---
+++ thread-and-process/play-with-thread-and-process.py
@@ -0,0 +1,25 @@
+from multiprocessing import Process
+import os
+import time
+
+def task(name):
+    print(f"[{name}] Process ID: {os.getpid()}")
+    print(f"[{name}] Starting work...")
+    time.sleep(2)
+    print(f"[{name}] Work done!")
+
+if __name__ == "__main__":
+    p1 = Process(target=task, args=("Task-1",))
+    p2 = Process(target=task, args=("Task-2",))
+
+    p1.start() # Start first Process
+    p1.join()
+
+    p2.start() # Start second process
+    p2.join()
+
+    print("Main process waiting for subprocesses to finish...")
+    #p1.join()
+    #p2.join()
+
+    print("Main process done.")
